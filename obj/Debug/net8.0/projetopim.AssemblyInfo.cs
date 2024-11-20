1f56590786547116e4a05a031021bf97784f0022
habibe <saaidmohanna@gmail.com>
2024-11-20 15:05:53 -0300

    feat: secondary commit

--- obj/Debug/net8.0/projetopim.AssemblyInfo.cs
+++ obj/Debug/net8.0/projetopim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetopim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e75de9a4830db8c05b200295e5f23be66944f7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a3b8d17bc5961ff370a4573110412e0595bfab")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetopim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetopim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
